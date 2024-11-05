defa3eb4a6970f38dd79392c53a5aa59eff9a427
huangjien <huangjien@gmail.com>
2024-11-05 15:54:01 +0000

    syntax issue

--- src/JenkinsPanel.ts
+++ src/JenkinsPanel.ts
@@ -67,7 +67,7 @@ export class JenkinsPanel {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Security-Policy" content="default-src 'self' style-src 'self' ; img-src ${webview.cspSource} https:; style-src ${webview.cspSource}; script-src 'nonce-${nonce}'; style-src-elem ${webview.cspSource} 'unsafe-inline'; font-src ${webview.cspSource}">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self'; style-src 'self' ; img-src ${webview.cspSource} https:; style-src ${webview.cspSource}; script-src 'nonce-${nonce}'; style-src-elem ${webview.cspSource} 'unsafe-inline'; font-src ${webview.cspSource}">
     <link rel="stylesheet" href="${stylesUri}">
     <title>Jenkins Logs Analysis</title>
   </head>
