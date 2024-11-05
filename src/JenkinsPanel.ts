0ae6d3f7eb03c7a7d38818e5cd268d3cde7045b1
huangjien <huangjien@gmail.com>
2024-11-05 18:00:20 +0000

    no reset.css

--- src/JenkinsPanel.ts
+++ src/JenkinsPanel.ts
@@ -68,9 +68,10 @@ export class JenkinsPanel {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Security-Policy" content="default-src 'none'; 
-    style-src 'unsafe-inline' '${stylesUri}' 'nonce-${nonce}' ; 
+    style-src 'unsafe-inline' '${webview.cspSource}' 'nonce-${nonce}' ; 
     img-src data:;  
-    script-src 'unsafe-inline' 'nonce-${nonce}'; ">
+    script-src 'unsafe-inline' 'nonce-${nonce}'; 
+    style-src-elem 'unsafe-inline' '${webview.cspSource}' ;">
     <link rel="stylesheet" href="${stylesUri}">
     <title>Jenkins Logs Analysis</title>
   </head>
