14bd3b24900d786fac67f0255d035d09c336ecfb
huangjien <huangjien@gmail.com>
2024-11-05 17:51:29 +0000

    no reset.css

--- src/JenkinsPanel.ts
+++ src/JenkinsPanel.ts
@@ -68,7 +68,7 @@ export class JenkinsPanel {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Security-Policy" content="default-src 'none'; 
-    style-src 'unsafe-inline' '${webview.cspSource}' 'nonce-${nonce}' ; 
+    style-src 'unsafe-inline' '${stylesUri}' 'nonce-${nonce}' ; 
     img-src data:;  
     script-src 'unsafe-inline' 'nonce-${nonce}'; ">
     <link rel="stylesheet" href="${stylesUri}">
