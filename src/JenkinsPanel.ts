58aaea422f4f9c2745a68a8403e65e11f13c108d
huangjien <huangjien@gmail.com>
2024-11-06 10:16:10 +0000

    look works?

--- src/JenkinsPanel.ts
+++ src/JenkinsPanel.ts
@@ -67,10 +67,13 @@ export class JenkinsPanel {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Security-Policy" content="default-src self ; 
-    style-src ${webview.cspSource} nonce-${nonce} ; script-src self ${webview.cspSource} ; 
-    img-src ${webview.cspSource} https:;   font-src ${webview.cspSource};
-    style-src-elem ${webview.cspSource} ;">
+    <meta http-equiv="Content-Security-Policy" content="default-src 'self';
+    style-src ${webview.cspSource} 'unsafe-inline';
+    script-src 'self' ${webview.cspSource};
+    img-src ${webview.cspSource} https:;
+    font-src ${webview.cspSource};
+    style-src-elem 'self' ${webview.cspSource} 'unsafe-inline';">
+
     <link rel="stylesheet" href="${stylesUri}">
     <title>Jenkins Logs Analysis</title>
   </head>
